4592b818f0f6d82d87358d0172f16dab71300cb8
NishantSwat <nayanishant.info@gmail.com>
2024-06-08 22:56:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const hexclr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 function App() {
+  const [clrCode, setClrCode] = useState("#5F9EA0");
+
+  const handleClrChange = () => {
+    let hexcolor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexcolor += hexclr[rndmNmbr()];
+    }
+    setClrCode(hexcolor);
+  };
+
+  const rndmNmbr = () => {
+    return Math.floor(Math.random() * hexclr.length);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapperMain" style={{ backgroundColor: clrCode }}>
+      <div className="wrapper">
+        <div className="top">
+          <h1 className="colorCode" /*style={{ color: clrCode}}*/>{clrCode}</h1>
+          <span>(Color Code)</span>
+        </div>
+        <div className="bottom">
+          <p>Click the button to change the background color.</p>
+          <button onClick={handleClrChange}>Click Here</button>
+        </div>
+      </div>
     </div>
   );
 }
